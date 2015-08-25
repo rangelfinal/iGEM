990588ea3558db25ed0ec4d46e36174083f1b30e
Vitor Terçariol Rangel <rangel.final@gmail.com>
2015-08-25 15:05:40 -0300

    Dropdowns and slick working

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$('.ui.dropdown').dropdown({
+  allowCategorySelection: true,
+  on: 'hover'
+});
+
+$('.banner.slider').slick({
+  dots: true,
+  adaptiveHeight: true
+});
+
+$('.sponsor.slider').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true
+});
+
+$('.ufscar.slider').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true
+});
